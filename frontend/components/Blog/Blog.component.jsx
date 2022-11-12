6e1c0fd6205d60c7b9cab9e7b710c2eacb4725c5
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2022-11-12 01:06:20 +0100

    feat: searchbar in blog

--- frontend/components/Blog/Blog.component.jsx
+++ frontend/components/Blog/Blog.component.jsx
@@ -1,14 +1,66 @@
 import posts from "../../services/posts";
 import styleBlog from "./Blog.module.scss";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function BlogComponent() {
+  //usado para mostrar barra de busqueda o boton de lupa
+  const [searchBar, setSearchBar] = useState(false);
+  //usado para guardar el criterio de busqueda
+  const [valueInput, setValueInput] = useState("");
+  //usado para filtrar los resultados de busqueda en el array de posts
+  const [postsOk, setPostsOk] = useState(posts);
+
+  //filtar
+  const search = (e) => {
+    setPostsOk(
+      posts.filter(
+        (post) =>
+          post.content.toUpperCase().includes(e.toUpperCase()) ||
+          post.title.toUpperCase().includes(e.toUpperCase())
+      )
+    );
+  };
+
+  //cada cambio en valueInput hace un filtrado en el array postOk, refrescando el filtrado constantemente
+  useEffect(() => {
+    search(valueInput);
+  }, [valueInput]);
+
   return (
     <div className="container_component">
       <div className="content_container">
         <div className={styleBlog.blog_title}>Lee los artículos de mi blog</div>
+        {/* barra de busqueda */}
+        {!searchBar && (
+          <div
+            className={styleBlog.search_button}
+            onClick={() => setSearchBar(true)}
+          >
+            <span class="material-symbols-outlined">search</span>
+          </div>
+        )}
+        {searchBar && (
+          <div className={styleBlog.search_bar}>
+            <span class="material-symbols-outlined">search</span>
+            <input
+              autoFocus
+              type="text"
+              placeholder="Busca por título o por contenido del post..."
+              value={valueInput}
+              onChange={(e) => setValueInput(e.target.value)}
+            />
+            <span
+              class="material-symbols-outlined"
+              onClick={() => (setSearchBar(false), setValueInput(""))}
+            >
+              close
+            </span>
+          </div>
+        )}
+        {/* barra de busqueda */}
         <div className={styleBlog.posts_list}>
-          {posts.map((post) => {
+          {postsOk.map((post) => {
             return (
               <div key={post.id} className={styleBlog.post_card}>
                 <Image
--- frontend/services/posts.js
+++ frontend/services/posts.js
@@ -1,64 +1,64 @@
 const posts = [
     {
         id: "1",
-        title: "Primer posts del blog",
-        content: "Patata de ejemplo del post. Este es el contenido de ejemplo del post.",
+        title: "Mi experiencia hasta lograr neuro",
+        content: "El Síndrome de Asperger es un trastorno del neurodesarrollo que afecta al funcionamiento social, a la comunicación y el lenguaje, a la capacidad motora y a las actividades e intereses del individuo que lo sufre, pese a que la inteligencia es normal. Existen distintas conductas que pueden indicar la presencia de este trastorno. En este texto se exponen diez señales características de esta patología.",
         created_at: "11/11/2022",
         image: "/images/brain.jpg",
         tags: ["neurociencia", "salud mental", "psicología"]
     },
     {
         id: "2",
-        title: "Primer posts del blog",
-        content: "Este es el contenido de  el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post.",
+        title: "Errores que la mayoría de neuro cometen",
+        content: "Elaboró la Teoría del Aprendizaje Social, en la que presta atención al aprendizaje por observación y a la importancia de las creencias de autoeficacia para la regulación del comportamiento humano. Además, introdujo también el concepto de determinismo recíproco, en el que explica que no sólo el medio ambiente influye en la persona, sino que la persona también es capaz de cambiar el medio ambiente.",
         created_at: "11/11/2022",
         image: "/images/brain.jpg",
         tags: ["neurociencia", "salud mental", "psicología"]
     },
     {
         id: "3",
-        title: "Primer posts del blog",
-        content: "Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post.",
+        title: "5 hábitos sobre neuro que aplico a mi día a día",
+        content: "De hecho, pocos casos de necrofilia son tan famosos como el de este médico alemán que decidió afincarse en Estados Unidos a finales de los años 20. Tanzler llevó su obsesión por una mujer más allá de la muerte, y fue capaz de exhumar el cadáver de la que consideraba la mujer de su vida, para continuar teniendo relaciones sexuales con ella. Puedes conocer su historia pinchando en el título.",
         created_at: "11/11/2022",
         image: "/images/brain.jpg",
         tags: ["neurociencia", "salud mental", "psicología"]
     },
     {
         id: "4",
-        title: "Primer posts del blog",
-        content: "Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post.",
+        title: "5 cosas que desearía haber sabido sobre neuro cuando empecé",
+        content: "Los psicólogos clásicos sentaron las bases de lo que hoy es la psicología, pero la ciencia psicológica necesita estar actualizada para satisfacer las necesidades de los seres humanos. Aquí tienes una lista con los 12 psicólogos más influyentes de hoy en día, con nombres de la talla de Albert Bandura, Elizabeth Loftus, Philip Zimbardo, Steven Pinker...",
         created_at: "11/11/2022",
         image: "/images/brain.jpg",
         tags: ["neurociencia", "salud mental", "psicología"]
     },
     {
         id: "5",
-        title: "Primer posts del blog",
-        content: "Este es el contenido dtenido de ejemplo del post. Este es el contenido de ejemplo del post.",
+        title: "Una introducción a neuro",
+        content: "Los textos pueden ser de gran ayuda, pero los documentales también pueden aportarnos conocimiento, además de entretenimiento. Para facilitarte la vida, nos hemos tomado la molestia de seleccionar quince documentales sobre psicología imprescindibles. Cada uno de ellos aborda una temática distinta, así que puedes escoger los que más te interesen. ¡Disfrútalos!",
         created_at: "11/11/2022",
         image: "/images/brain.jpg",
         tags: ["neurociencia", "salud mental", "psicología"]
     },
     {
         id: "6",
-        title: "Primer posts del blog",
-        content: "Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post.",
+        title: "Mi presentación sobre neuro que di en el evento SummerNeuro",
+        content: "A pesar de que estos individuos quieren mostrar una gran seguridad en sí mismas, eso es solo apariencia, lo que se conoce como una falsa autoconfianza. Por tanto, ¿cómo son realmente las personas egocéntricas? ¿Cuáles son sus rasgos más característicos? Descúbrelo en este artículo.",
         created_at: "11/11/2022",
         image: "/images/brain.jpg",
         tags: ["neurociencia", "salud mental", "psicología"]
     },
     {
         id: "7",
-        title: "Primer posts del blog",
-        content: "Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post.",
+        title: "Mi predicción sobre la industria de neuro para los próximos años",
+        content: "Es decir, que a través de la interacción amistosa entre el gato y la persona es posible superar situaciones de estrés, ansiedad o un bajo estado de ánimo. Además, los gatos pueden ayudarte a mejorar tu salud física y tu calidad de vida. Así que, si quieres saber más sobre los beneficios de la gatoterapia, te invitamos a que leas esta publicación.",
         created_at: "11/11/2022",
         image: "/images/brain.jpg",
         tags: ["neurociencia", "salud mental", "psicología"]
     },
     {
         id: "8",
-        title: "Primer posts del blog",
-        content: "Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post. Este es el contenido de ejemplo del post.",
+        title: "Mi objetivo acerca de la neuro y cómo lo conseguiré",
+        content: "Una vez finalizado el instituto, muchos jóvenes se plantean seguir sus estudios para formarse como profesionales de esta disciplina. Pero, ¿cuáles son los “pros” y “contras” de esta carrera? En este artículo se explican las ventajas e incontinentes de cursar el Grado en Psicología, para que puedas conocer varios detalles importantes sobre las salidas profesionales y sobre el currículo académico. Lectura obligatoria.",
         created_at: "11/11/2022",
         image: "/images/brain.jpg",
         tags: ["neurociencia", "salud mental", "psicología"]
